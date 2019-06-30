791842416c59a9bcf68f822d4488f8b06a4e2f76
Натали <t.natalie14@gmail.com>
2019-07-01 00:13:11 +0400

    добавила запросы к серверу

---
+++ js/backend.js
@@ -0,0 +1,85 @@
+'use strict';
+
+(function () {
+
+  var Url = {
+    GET: 'https://js.dump.academy/code-and-magick/data',
+    POST: 'https://js.dump.academy/code-and-magick'
+  };
+
+  var TIMEOUT = 10000;
+
+  var OK_STATUS = 200;
+
+  function getErrorMesage(error) {
+    switch (error) {
+      case 500:
+        return 'Internal Server Error — произошла внутренняя ошибка';
+      case 404:
+        return 'Not Found — запрашиваемый ресурс не найден';
+      case 400:
+        return 'Bad Request — неправильный запрос';
+      case 301:
+        return 'Moved Permanently — ресурс переехал навсегда';
+      case 307:
+        return 'Temporary Redirect — ресурс переехал временно';
+      default:
+        return 'Ошибка подключения, попробуйте позже';
+    }
+  }
+
+  window.backend = {
+
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === OK_STATUS) {
+          onLoad(xhr.response);
+        } else {
+          onError(getErrorMesage(xhr.status));
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError(getErrorMesage(xhr.status));
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError(getErrorMesage(xhr.status));
+      });
+
+      xhr.timeout = TIMEOUT;
+      xhr.open('GET', Url.GET);
+      xhr.send();
+    },
+
+    save: function (data, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.timeout = TIMEOUT;
+      xhr.open('POST', Url.POST);
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === OK_STATUS) {
+          onLoad(xhr.response);
+        } else {
+          onError(getErrorMesage(xhr.status));
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError(getErrorMesage(xhr.status));
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError(getErrorMesage(xhr.status));
+      });
+
+      xhr.send(data);
+    }
+
+  };
+})();
--- js/dialog.js
+++ js/dialog.js
@@ -2,34 +2,75 @@
 
 (function () {
 
+  var QUANTITY = 4;
+
   var setup = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
   var setupIcon = setupOpen.querySelector('.setup-open-icon');
   var setupUserName = setup.querySelector('.setup-user-name');
+  var form = setup.querySelector('.setup-wizard-form');
+  var setupButton = setup.querySelector('.setup-submit');
+
+  function renderWizard(wizard) {
+    var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+
+    return wizardElement;
+  }
+
+  function successHandler(wizards) {
+    var similarListElement = document.querySelector('.setup-similar-list');
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < QUANTITY; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+
+    setup.querySelector('.setup-similar').classList.remove('hidden');
+  }
+
+  function errorHandler(errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.classList.add('error-js');
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  }
 
   // функции показа окна настроек
 
   function openSetup() {
     setup.classList.remove('hidden');
     document.addEventListener('keydown', onEscPress);
+    window.backend.load(successHandler, errorHandler);
   }
 
   function closeSetup() {
+    var error = document.querySelector('.error-js');
+
     setup.classList.add('hidden');
     setup.removeAttribute('style');
+
+    if (error !== null) {
+      error.remove();
+      setupButton.disabled = false;
+    }
     document.removeEventListener('keydown', onEscPress);
   }
 
   // функции обработчки событий показа окна настроек
-
-  function onSetupOpenClick() {
-    openSetup();
-  }
-
-  function onSetupCloseClick() {
-    closeSetup();
-  }
 
   function onSetupIconEnterPress(evt) {
     window.util.isEnterEvent(evt, openSetup);
@@ -49,9 +90,29 @@
     }
   }
 
-  setupOpen.addEventListener('click', onSetupOpenClick);
-  setupClose.addEventListener('click', onSetupCloseClick);
+  // функция обработчик события отправки формы
+
+  function onFormSubmit(evt) {
+    var error = document.querySelector('.error-js');
+
+    setupButton.disabled = true;
+
+    window.backend.save(new FormData(form), function () {
+      setup.classList.add('hidden');
+      setupButton.disabled = false;
+    }, errorHandler);
+
+    if (error !== null) {
+      error.remove();
+    }
+
+    evt.preventDefault();
+  }
+
+  setupOpen.addEventListener('click', openSetup);
+  setupClose.addEventListener('click', closeSetup);
   setupClose.addEventListener('keydown', onSetupCloseEnterPress);
   setupIcon.addEventListener('keydown', onSetupIconEnterPress);
+  form.addEventListener('submit', onFormSubmit);
 
 })();
